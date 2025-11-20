2c49dca45dba13894c85037043fa54b5b108c611
William Lampley <wlampley@outlook.com>
2025-11-20 09:52:53 -0600

    added command line argument and config file handeling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,63 @@
 #include <iostream>
-#include "memory_simulator.h"
+#include <string>
+#include <fstream>
+#include "./../include/memory_simulator.h"
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    std::cout << "HybridMemSim - Hybrid Memory Simulator" << std::endl;
+typedef struct config{
 
+    int bank_count;
+    int bank_size;
+    int initial_value;
+
+} Config;
+
+int read_config(string file_path, Config * config){
+
+    ifstream file(file_path);
+        if( !(file.is_open()) ){
+            cerr << "ERROR, INVALID CONFIG FILE PATH!!"<< endl;
+            return 1;
+        }
+    cout << "Reading config file" << endl;
+    string label;
+
+    file >> label >> label >> config->bank_count;
+    file >> label >> label >> config->bank_size;
+    file >> label >> label >> config->initial_value;
+
+    file.close();
+    return 0;
+
+}
+
+
+
+
+int main(int argc, char* argv[]) {
+    cout << "HybridMemSim - Hybrid Memory Simulator" << endl;
+
+    //parse command line arguments
+        if (argc != 2){
+            cout << "Argument Error" << endl;
+            //cout << "Usage: <number of banks > <bank size in words> <path to config file>" << endl;
+            cout << "Usage: <path to config file>" << endl;
+            return 1;
+        }
+        //build dram array
+        // int bank_count = stoi(argv[1]);
+        // int bank_size = stoi(argv[2]);
+        //string config_path = argv[3]
+        string config_path = argv[1];
+        Config config;
+    
+    if (read_config(config_path,&config)) return 1;
+
+    MemorySimulator memorysim;
+
+
+    
     // TODO: Parse command line arguments
     // TODO: Load configuration
     // TODO: Initialize simulator
--- src/memory_simulator.cpp
+++ src/memory_simulator.cpp
@@ -1,29 +1,50 @@
-#include "memory_simulator.h"
+#include "./../include/memory_simulator.h"
 #include <iostream>
+#include <vector>
 
-MemorySimulator::MemorySimulator() {
-    // Constructor
-}
+#define initial_value 0 
 
-MemorySimulator::~MemorySimulator() {
-    // Destructor
-}
 
-void MemorySimulator::initialize() {
-    // TODO: Initialize DRAM subsystem
-    // TODO: Initialize Flash memory (if hybrid mode)
-    // TODO: Set up queues and buffers
-}
+using namespace std;
 
-void MemorySimulator::run() {
-    // TODO: Main simulation loop
-    // TODO: Process memory requests
-    // TODO: Handle DRAM row-buffer hits/misses
-    // TODO: Handle refresh cycles
-}
+class MemorySimulator{
 
-void MemorySimulator::printStats() {
-    // TODO: Output latency statistics
-    // TODO: Output throughput metrics
-    // TODO: Output hit/miss ratios
-}
+public:
+    MemorySimulator::MemorySimulator() {
+
+    }
+
+    MemorySimulator::~MemorySimulator() {
+        // Destructor
+    }
+
+    void MemorySimulator::initialize() {
+        // for (auto& row : mem_vec) {
+        //     fill(row.begin(), row.end(), initial_value);
+        // }
+        // TODO: Initialize DRAM subsystem
+        // TODO: Initialize Flash memory (if hybrid mode)
+        // TODO: Set up queues and buffers
+    }
+
+    void MemorySimulator::run() {
+        // TODO: Main simulation loop
+        // TODO: Process memory requests
+        // TODO: Handle DRAM row-buffer hits/misses
+        // TODO: Handle refresh cycles
+    }
+
+    void MemorySimulator::printStats() {
+        // TODO: Output latency statistics
+        // TODO: Output throughput metrics
+        // TODO: Output hit/miss ratios
+    }
+};
+
+// void clear_memory(vector<int>& arr){
+
+//     for (auto& row : mem_vec) {
+//         fill(row.begin(), row.end(), initial_value);
+//     }
+
+// }
